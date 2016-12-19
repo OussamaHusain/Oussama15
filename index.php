7f7675aec26d8a0d2e411777a46c0d1470f631ee
Oussama <Oussama@192.168.10.219>
2016-12-19 18:00:53 +0100

    premier commit en local

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Projet GIT ISFCE DvpWeb</h1>
+        <p>lUNDI 19 DECEMBRE 2016.</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
